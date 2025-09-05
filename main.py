b0b8e0b5f09d3a7f6048ad9bea60aa82920a5c1c
Laywood <pomilli911@gmail.com>
2025-09-06 00:37:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler
 
-# Замените на ваш токен
+# Ваш токен
 TOKEN = "8490585937:AAGa8Po7KC4v6vhaQHSJficyrL8mPjhorQk"
 
 async def start(update: Update, context):
     """Обработчик команды /start"""
-    # Текст сообщения
     message_text = "👋 Привет! Добро пожаловать в Study Vibe! 📚 Я твой персональный помощник с домашними заданиями. Нажми кнопку - Open, чтобы открыть Study Vibe."
     
-    # Отправляем сообщение с кнопкой
-    await update.message.reply_text(
-        text=message_text,
-        reply_markup=reply_markup
-    )
+    await update.message.reply_text(message_text)
 
 def main():
-    if not TOKEN:
-        print("Ошибка: BOT_TOKEN не найден!")
-        return
-    
     app = Application.builder().token(TOKEN).build()
     
-    # Добавляем обработчики
+    # Добавляем обработчик команды /start
     app.add_handler(CommandHandler("start", start))
     
-    print("Study Vibe бот запущен на Railway...")
+    print("Study Vibe бот запущен...")
     app.run_polling()
 
 if __name__ == "__main__":
